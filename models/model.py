a6b706390ea626a7b81202a3c8c97d68e753bc7f
gunn <gunnkalra10@gmail.com>
2026-06-03 10:02:24 +0530

    Initial commit

---
+++ models/model.py
@@ -0,0 +1,11 @@
+import joblib
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv('data/house_data.csv')
+x = data[['area']]
+y = data['price']
+model = LinearRegression()
+model.fit(x, y)
+joblib.dump(model,'model.pkl')
+print("Model trained and saved successfully.")
